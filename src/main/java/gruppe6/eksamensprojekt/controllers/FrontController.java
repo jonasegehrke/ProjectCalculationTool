c0048eb8edaf61c6b3596cffaff864eec82401db
Jimmi Pisalita <mig.jimmi@gmail.com>
2021-05-04 13:04:17 +0200

    Made endpoint for dashboard and added html page

--- src/main/java/gruppe6/eksamensprojekt/controllers/FrontController.java
+++ src/main/java/gruppe6/eksamensprojekt/controllers/FrontController.java
@@ -1,7 +1,13 @@
 package gruppe6.eksamensprojekt.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FrontController {
+
+    @GetMapping
+    public String dashboard(){
+        return index.html;
+    }
 }
