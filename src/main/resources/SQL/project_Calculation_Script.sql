65e78ec962ea530cf970c79b79840c0a1b3f3921
Jonas Gehrke <jonasegehrke@gmail.com>
2021-05-20 16:03:37 +0200

    Added test material to script

--- src/main/resources/SQL/project_Calculation_Script.sql
+++ src/main/resources/SQL/project_Calculation_Script.sql
@@ -38,7 +38,15 @@ FOREIGN KEY (task_id) references task(id),
 FOREIGN KEY (employee_id) references employee(id)
 );
 
+INSERT INTO project (title) VALUES("MyFirstProject");
+INSERT INTO employee (emp_name, job_title) VALUES("Jonas Emil Gehrke", "Software Developer");
+INSERT INTO employee (emp_name, job_title) VALUES("Jimmi Paw Pisalita", "Software Developer");
+INSERT INTO employee (emp_name, job_title) VALUES("Jacob Gade Harder", "System Designer");
+INSERT INTO employee (emp_name, job_title) VALUES("Andreas Holm Andersen", "System Designer");
+
 
 SELECT * FROM subtask;
 SELECT * FROM task;
 SELECT * FROM project;
+SELECT * FROM employee;
+
