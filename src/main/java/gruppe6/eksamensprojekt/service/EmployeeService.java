96e2ac38025166e7decabd21b54c92c22dbffea3
AndreasGargulak <andr6230@stud.kea.dk>
2021-05-21 09:10:59 +0200

    Added string to EmployeeService

--- src/main/java/gruppe6/eksamensprojekt/service/EmployeeService.java
+++ src/main/java/gruppe6/eksamensprojekt/service/EmployeeService.java
@@ -48,7 +48,10 @@ public class EmployeeService {
             if(employeeList.get(i).getId() == employeeId){
                 employeeMapper.deleteEmployee(employeeList.get(i));
             }
+            String employee = "hej";
         }
     }
+
+
 
 }
