575eb72194ac1e5961bb1cff4dd4e237294360f5
Jonas Gehrke <jonasegehrke@gmail.com>
2021-05-21 10:53:36 +0200

    Fixed something idk

--- src/main/resources/Static/js/script.js
+++ src/main/resources/Static/js/script.js
@@ -2,6 +2,8 @@ function openNav() {
     document.getElementById("employeenav").style.width = "200px";
     document.getElementById("wrapper").style.marginLeft = "-200px";
     document.getElementById("info").style.width = "90%";
+
+
     document.getElementById("closebtn").style.display = "block";
     document.getElementById("openbtn").style.display = "none";
 }
@@ -9,7 +11,9 @@ function openNav() {
 function closeNav() {
     document.getElementById("employeenav").style.width = "0";
     document.getElementById("wrapper").style.marginLeft= "0";
-    document.getElementById("info").style.width = "100%";
+    document.getElementById("info").style.width = null;
+
+
     document.getElementById("closebtn").style.display = "none";
     document.getElementById("openbtn").style.display = "block";
 }
