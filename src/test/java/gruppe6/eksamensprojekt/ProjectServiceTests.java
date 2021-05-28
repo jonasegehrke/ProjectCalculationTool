dd05c08ce24e15380b0b9f09bb75bd9da2108923
Jimmi Pisalita <mig.jimmi@gmail.com>
2021-05-28 13:41:50 +0200

    Made unit test for create, read and delete project

--- src/test/java/gruppe6/eksamensprojekt/ProjectServiceTests.java
+++ src/test/java/gruppe6/eksamensprojekt/ProjectServiceTests.java
@@ -1,6 +1,8 @@
 package gruppe6.eksamensprojekt;
 
+import gruppe6.eksamensprojekt.domain.model.Employee;
 import gruppe6.eksamensprojekt.domain.model.Project;
+import gruppe6.eksamensprojekt.service.EmployeeService;
 import gruppe6.eksamensprojekt.service.ProjectService;
 import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,36 +11,52 @@ import org.springframework.util.Assert;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 @SpringBootTest
 class ProjectServiceTests {
 
     @Test
-    void testRenderProjectList() {
+    void testReadProject() {
         //Arrange
             ProjectService projectService = new ProjectService();
+            Project project;
         //Act
-            ArrayList result = new ArrayList();
+            projectService.createProject("test");
+            project = projectService.readProject(projectService.getCurrentProjectId());
         //Assert
-            assertEquals(result, projectService.renderProjectList());
+            assertEquals("test", project.getTitle());
     }
 
     @Test
     void testCreateProject() {
         //Arrange
-        ProjectService projectService = new ProjectService();
-        Project projectX = new Project("test", 0);
+            ProjectService projectService = new ProjectService();
+            Project project;
         //Act
-        Project projectY = projectService.readProject(projectX.getId());
+            projectService.createProject("test");
+            project = projectService.readProject(projectService.getCurrentProjectId());
         //Assert
-        assertEquals(projectX, projectY);
+            assertEquals("test", project.getTitle());
     }
-
 
     @Test
-    void testReadProject() {
-
+    void testDeleteProject() throws Exception {
+        //Arrange
+            ProjectService projectService = new ProjectService();
+            EmployeeService employeeService = new EmployeeService();
+            Project project;
+        //Act
+            projectService.createProject("testDeleteProject");
+            projectService.readTaskList(projectService.getCurrentProjectId());
+            projectService.renderProjectList();
+            projectService.deleteProject(projectService.getCurrentProjectId(), employeeService);
+            project = projectService.readProject(projectService.getCurrentProjectId());
+        //Assert
+            assertNull(project);
     }
+
+
 
     /**
      *
